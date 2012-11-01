487492212fea83efc15b305f06ec6f2f549f3030
madvirus <madvirus@madvirus.net>
2012-11-01 23:24:57 +0900

    테스트 코드에서 VideoFormat 검증 위한 헬퍼 추가 및 테스트 코드에서 사용하도록 반영

--- src/test/java/org/chimi/s4t/infra/ffmpeg/VideoConverterTest.java
+++ src/test/java/org/chimi/s4t/infra/ffmpeg/VideoConverterTest.java
@@ -1,5 +1,7 @@
 package org.chimi.s4t.infra.ffmpeg;
 
+import java.io.File;
+
 import org.chimi.s4t.domain.job.AudioCodec;
 import org.chimi.s4t.domain.job.OutputFormat;
 import org.chimi.s4t.domain.job.VideoCodec;
@@ -10,18 +12,26 @@ import com.xuggle.mediatool.ToolFactory;
 
 public class VideoConverterTest {
 
+	private static final int WIDTH = 160;
+	private static final int HEIGHT = 120;
+	private static final int BITRATE = 150;
+	private static final String SOURCE_FILE = "src/test/resources/sample.avi";
+	private static final String TRANSCODED_FILE = "target/sample.mp4";
+
 	@Test
 	public void transcode() {
-		IMediaReader reader = ToolFactory
-				.makeReader("src/test/resources/sample.avi");
+		IMediaReader reader = ToolFactory.makeReader(SOURCE_FILE);
 
-		OutputFormat outputFormat = new OutputFormat(160, 120, 150,
+		OutputFormat outputFormat = new OutputFormat(WIDTH, HEIGHT, BITRATE,
 				VideoCodec.H264, AudioCodec.AAC);
-		VideoConverter writer = new VideoConverter("target/sample.mp4", reader,
+		VideoConverter writer = new VideoConverter(TRANSCODED_FILE, reader,
 				outputFormat);
 		reader.addListener(writer);
 		while (reader.readPacket() == null)
 			do {
 			} while (false);
+
+		VideoFormatVerifier.verifyVideoFormat(outputFormat, new File(
+				TRANSCODED_FILE));
 	}
 }
---
+++ src/test/java/org/chimi/s4t/infra/ffmpeg/VideoFormatVerifier.java
@@ -0,0 +1,84 @@
+package org.chimi.s4t.infra.ffmpeg;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.chimi.s4t.domain.job.OutputFormat;
+
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IStream;
+import com.xuggle.xuggler.IStreamCoder;
+
+public class VideoFormatVerifier {
+
+	public static void verifyVideoFormat(OutputFormat expectedFormat,
+			File videoFile) {
+		new VideoFormatVerifier(expectedFormat, videoFile).verify();
+	}
+
+	private IContainer container;
+	private int width;
+	private int height;
+	private ICodec.ID videoCodec;
+	private ICodec.ID audioCodec;
+
+	private OutputFormat expectedFormat;
+	private File videoFile;
+
+	public VideoFormatVerifier(OutputFormat expectedFormat, File videoFile) {
+		this.expectedFormat = expectedFormat;
+		this.videoFile = videoFile;
+	}
+
+	public void verify() {
+		try {
+			makeContainer();
+			extractMetaInfoOfVideo();
+			assertVideoFile();
+		} finally {
+			closeContainer();
+		}
+	}
+
+	private void makeContainer() {
+		container = IContainer.make();
+		int openResult = container.open(videoFile.getAbsolutePath(),
+				IContainer.Type.READ, null);
+		if (openResult < 0) {
+			throw new RuntimeException("Xuggler file open failed: "
+					+ openResult);
+		}
+	}
+
+	private void extractMetaInfoOfVideo() {
+		int numStreams = container.getNumStreams();
+		for (int i = 0; i < numStreams; i++) {
+			IStream stream = container.getStream(i);
+			IStreamCoder coder = stream.getStreamCoder();
+			if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO) {
+				audioCodec = coder.getCodecID();
+			} else if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
+				videoCodec = coder.getCodecID();
+				width = coder.getWidth();
+				height = coder.getHeight();
+			}
+		}
+	}
+
+	private void closeContainer() {
+		if (container != null)
+			container.close();
+	}
+
+	private void assertVideoFile() {
+		assertEquals(expectedFormat.getWidth(), width);
+		assertEquals(expectedFormat.getHeight(), height);
+		assertEquals(expectedFormat.getVideoCodec(),
+				CodecValueConverter.toDomainVideoCodec(videoCodec));
+		assertEquals(expectedFormat.getAudioCodec(),
+				CodecValueConverter.toDomainAudioCodec(audioCodec));
+	}
+
+}
