4cf10024ec4433066ac4f456b3fffb23d78c7c6e
madvirus <madvirus@10.50.1.150>
2012-11-06 18:44:56 +0900

    VideoConverter 정리 / OutputFormat에 코덱 없는 생성자 추가

--- src/test/java/org/chimi/s4t/infra/ffmpeg/VideoConverterTest.java
+++ src/test/java/org/chimi/s4t/infra/ffmpeg/VideoConverterTest.java
@@ -6,6 +6,7 @@ import org.chimi.s4t.domain.job.AudioCodec;
 import org.chimi.s4t.domain.job.Container;
 import org.chimi.s4t.domain.job.OutputFormat;
 import org.chimi.s4t.domain.job.VideoCodec;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.xuggle.mediatool.IMediaReader;
@@ -17,22 +18,54 @@ public class VideoConverterTest {
 	private static final int HEIGHT = 120;
 	private static final int BITRATE = 150;
 	private static final String SOURCE_FILE = "src/test/resources/sample.avi";
-	private static final String TRANSCODED_FILE = "target/sample.mp4";
+	private IMediaReader reader;
+	private OutputFormat outputFormat;
+	private String outputFile;
+
+	@Before
+	public void setup() {
+		reader = ToolFactory.makeReader(SOURCE_FILE);
+	}
 
 	@Test
 	public void transcode() {
-		IMediaReader reader = ToolFactory.makeReader(SOURCE_FILE);
+		initOutput(Container.MP4, VideoCodec.H264, AudioCodec.AAC,
+				"target/sample.mp4");
+		testVideoConverter();
+	}
 
-		OutputFormat outputFormat = new OutputFormat(WIDTH, HEIGHT, BITRATE,
-				Container.MP4, VideoCodec.H264, AudioCodec.AAC);
-		VideoConverter writer = new VideoConverter(TRANSCODED_FILE, reader,
+	@Test
+	public void transcodeWithOnlyContainer() {
+		initOutput(Container.AVI, "target/sample.avi");
+		testVideoConverter();
+	}
+
+	private void initOutput(Container outputContainer, String outputFileName) {
+		initOutput(outputContainer, null, null, outputFileName);
+		outputFile = outputFileName;
+	}
+
+	private void initOutput(Container outputContainer, VideoCodec videoCodec,
+			AudioCodec audioCodec, String outputFileName) {
+		if (videoCodec == null && audioCodec == null) {
+			outputFormat = new OutputFormat(WIDTH, HEIGHT, BITRATE,
+					outputContainer);
+		} else {
+			outputFormat = new OutputFormat(WIDTH, HEIGHT, BITRATE,
+					outputContainer, videoCodec, audioCodec);
+		}
+		outputFile = outputFileName;
+	}
+
+	private void testVideoConverter() {
+		VideoConverter writer = new VideoConverter(outputFile, reader,
 				outputFormat);
 		reader.addListener(writer);
 		while (reader.readPacket() == null)
 			do {
 			} while (false);
 
-		VideoFormatVerifier.verifyVideoFormat(outputFormat, new File(
-				TRANSCODED_FILE));
+		VideoFormatVerifier.verifyVideoFormat(outputFormat,
+				new File(outputFile));
 	}
 }
