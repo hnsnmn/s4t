75072eaf485c427bbe24e8c989d317f0e2a59837
madvirus <madvirus@madvirus.net>
2012-11-13 21:13:01 +0900

    TranscodingRunner 작업 중

--- src/main/java/org/chimi/s4t/application/transcode/TranscodingRunner.java
+++ src/main/java/org/chimi/s4t/application/transcode/TranscodingRunner.java
@@ -1,10 +1,32 @@
 package org.chimi.s4t.application.transcode;
 
+import org.chimi.s4t.domain.job.Job;
+import org.chimi.s4t.domain.job.JobRepository;
+
 public class TranscodingRunner {
 
+	private TranscodingService transcodingService;
+	private JobRepository jobRepository;
+
+	public TranscodingRunner(TranscodingService transcodingService,
+			JobRepository jobRepository) {
+		this.transcodingService = transcodingService;
+		this.jobRepository = jobRepository;
+	}
+
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		Job job = getNextJob();
+		runTranscoding(job);
+	}
+
+	private Job getNextJob() {
+		return jobRepository.findEldestJobOfCreatedState();
+	}
+
+	private void runTranscoding(Job job) {
+		if (job == null)
+			return;
+		transcodingService.transcode(job.getId());
 	}
 
 }
--- src/test/java/org/chimi/s4t/application/transcode/TranscodingRunnerTest.java
+++ src/test/java/org/chimi/s4t/application/transcode/TranscodingRunnerTest.java
@@ -3,6 +3,8 @@ package org.chimi.s4t.application.transcode;
 import static org.mockito.Mockito.*;
 
 import org.chimi.s4t.domain.job.Job;
+import org.chimi.s4t.domain.job.JobRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -13,12 +15,35 @@ public class TranscodingRunnerTest {
 
 	@Mock
 	private Job job;
-	
-	@Test
-	public void runTranscodingWhenJobIsExists() {
-		TranscodingRunner runner = new TranscodingRunner();
-		runner.run();
-		
-		//verify(job, only()).transcode(transcoder, thumbnailExtractor);
+	@Mock
+	private TranscodingService transcodingService;
+	@Mock
+	private JobRepository jobRepository;
+
+	private TranscodingRunner runner;
+
+	@Before
+	public void setup() {
+		runner = new TranscodingRunner(transcodingService, jobRepository);
 	}
+
+	@Test
+	public void runTranscodingSuccessfullyWhenJobIsExists() {
+		when(job.getId()).thenReturn(1L);
+		when(jobRepository.findEldestJobOfCreatedState()).thenReturn(job);
+
+		runner.run();
+
+		verify(transcodingService, only()).transcode(1L);
+	}
+
+	@Test
+	public void dontRunTranscodingWhenJobIsNotExists() {
+		when(jobRepository.findEldestJobOfCreatedState()).thenReturn(null);
+
+		runner.run();
+
+		verify(transcodingService, never()).transcode(anyLong());
+	}
+
 }
